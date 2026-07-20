6fa50ba60de583723a65238317b4807c70498095
nahuel2003 <nahuelmarquez2003@gmail.com>
2026-07-20 18:35:09 -0300

    V1

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+var CACHE = "cv-v1";
+
+var PRECACHE = [
+  "/",
+  "/index.html",
+  "/colors.css",
+  "/manifest.json",
+  "/images/logo.png",
+  "/images/portrait.webp",
+  "/images/hero-background.webp",
+  "/images/social-preview.webp"
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(PRECACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(res) {
+        if (e.request.url.indexOf("http") === 0) {
+          var copy = res.clone();
+          caches.open(CACHE).then(function(c) { c.put(e.request, copy); });
+        }
+        return res;
+      });
+    })
+  );
+});
